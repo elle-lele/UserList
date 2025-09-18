0bef12ff7cb6a15788f160a9e6b7da039e0ad4df
elle-lele <naphatsanan199225@gmail.com>
2025-09-18 13:40:07 +0700

    dark mode 2

--- js/darkmodetoggle.js
+++ js/darkmodetoggle.js
@@ -2,47 +2,15 @@
   'use strict'
 
   const storedTheme = localStorage.getItem('theme')
-  const getPreferredTheme = () => {
-    if (storedTheme) {
-      return storedTheme
-    }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-  }
+  const defaultTheme = storedTheme ? storedTheme : 'light'
+  document.documentElement.setAttribute('data-bs-theme', defaultTheme)
 
-  const setTheme = function (theme) {
-    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark')
-    } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
-    }
-  }
-
-  setTheme(getPreferredTheme())
-
-  const showActiveTheme = (theme) => {
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    if (!btnToActive) return
-    const btns = document.querySelectorAll('[data-bs-theme-value]')
-    btns.forEach(btn => btn.classList.remove('active'))
-    btnToActive.classList.add('active')
-  }
-
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    if (storedTheme !== 'light' && storedTheme !== 'dark') {
-      setTheme(getPreferredTheme())
-    }
-  })
-
-  window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
-    const themeSwitcher = document.querySelectorAll('[data-bs-theme-value]')
-    themeSwitcher.forEach(toggle => {
-      toggle.addEventListener('click', () => {
-        const theme = toggle.getAttribute('data-bs-theme-value')
-        localStorage.setItem('theme', theme)
-        setTheme(theme)
-        showActiveTheme(theme)
-      })
+  const themeToggleButtons = document.querySelectorAll('[data-bs-theme-value]')
+  themeToggleButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const mode = btn.getAttribute('data-bs-theme-value')
+      document.documentElement.setAttribute('data-bs-theme', mode)
+      localStorage.setItem('theme', mode)
     })
   })
 })()
